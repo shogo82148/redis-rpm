e0833f407fbde132e5ea99e612997be3b9ddda75
Ichinose Shogo <shogo82148@gmail.com>
2021-01-13 21:18:23 +0900

    add upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -11,29 +11,27 @@ ROOT=$(cd "$(dirname "$0")/../" && pwd)
 
 SPEC_FILE="$ROOT/rpmbuild/SPECS/redis.spec"
 
+DATE=$(date '+%Y-%m-%dT%H:%M:%S%z')
 VERSION=$(sed -ne 's/^Version:[[:space:]]*\([.0-9]*\).*/\1/p' "$SPEC_FILE")
 RELEASE=$(sed -ne 's/^Release:[[:space:]]*\([.0-9]*\).*/\1/p' "$SPEC_FILE")
 
-for PACKAGE in redis redis-debuginfo redis-devel redis-doc redis-trib
-do
-    : create a new version
-    curl -u "$USER:$BINTRAY_API_KEY" \
-        "$BINTRAY_API/packages/$SUBJECT/$REPO/$PACKAGE/versions" \
-        -H 'Content-Type: application/json' \
-        -d "$(
-            jq -nc \
-                --arg version "$VERSION-$RELEASE" \
-                --arg desc "Automated release from main" \
-                --arg released "$(date '+%Y-%m-%dT%H:%M:%S%z')" \
-                '{ name: $version, released: $released }'
-            )"
-
-    : upload RPM files
-
-    for RPM in "$ROOT/$DISTRO.build/$PACKAGE-$VERSION-$RELEASE".*.rpm
-    do
+function upload () {
+    PACKAGE=$1
+    RPM=$2
+    UPLOAD_PATH=$3
+    if [[ -f "$RPM" ]]; then
         curl -u "$USER:$BINTRAY_API_KEY" \
-            "$BINTRAY_API/content/$SUBJECT/$REPO/$DISTRO/2/x86_64/$(basename "$RPM")" \
+            "$BINTRAY_API/packages/$SUBJECT/$REPO/$PACKAGE/versions" \
+            -H 'Content-Type: application/json' \
+            -d "$(
+                jq -nc \
+                    --arg version "$VERSION-$RELEASE" \
+                    --arg desc "Automated release from main" \
+                    --arg released "$DATE" \
+                    '{ name: $version, released: $released }'
+                )"
+        curl -u "$USER:$BINTRAY_API_KEY" \
+            "$BINTRAY_API/content/$SUBJECT/$REPO/$UPLOAD_PATH/$(basename "$RPM")" \
             -H "X-Bintray-Package: $PACKAGE" \
             -H "X-Bintray-Version: $VERSION-$RELEASE" \
             -H "X-Bintray-Publish: 1" \
@@ -41,5 +39,21 @@ do
             -H "Content-Type: application/octet-stream" \
             -XPUT \
             --data-binary "@$RPM"
-    done
-done
+    fi
+}
+
+if [[ "$DISTRO" = "amazonlinux2" ]]; then
+    upload redis "$ROOT/amazonlinux2.build/RPMS/x86_64/redis-$VERSION-$RELEASE.amzn2.x86_64.rpm" "amazonlinux2/2/x86_64"
+    upload redis-debuginfo "$ROOT/amazonlinux2.build/RPMS/x86_64/redis-debuginfo-$VERSION-$RELEASE.amzn2.x86_64.rpm" "amazonlinux2/2/x86_64"
+    upload redis-devel "$ROOT/amazonlinux2.build/RPMS/x86_64/redis-devel-$VERSION-$RELEASE.amzn2.x86_64.rpm" "amazonlinux2/2/x86_64"
+    upload redis-doc "$ROOT/amazonlinux2.build/RPMS/noarch/redis-doc-$VERSION-$RELEASE.amzn2.x86_64.rpm" "amazonlinux2/2/noarch"
+    upload redis-trib "$ROOT/amazonlinux2.build/RPMS/noarch/redis-trib-$VERSION-$RELEASE.amzn2.x86_64.rpm" "amazonlinux2/2/noarch"
+fi
+
+if [[ "$DISTRO" = "centos7" ]]; then
+    upload redis "$ROOT/centos7.build/RPMS/x86_64/redis-$VERSION-$RELEASE.amzn2.x86_64.rpm" "centos/7/x86_64"
+    upload redis-debuginfo "$ROOT/centos7.build/RPMS/x86_64/redis-debuginfo-$VERSION-$RELEASE.amzn2.x86_64.rpm" "centos/7/x86_64"
+    upload redis-devel "$ROOT/centos7.build/RPMS/x86_64/redis-devel-$VERSION-$RELEASE.amzn2.x86_64.rpm" "centos/7/x86_64"
+    upload redis-doc "$ROOT/centos7.build/RPMS/noarch/redis-doc-$VERSION-$RELEASE.amzn2.x86_64.rpm" "centos/7/noarch"
+    upload redis-trib "$ROOT/centos7.build/RPMS/noarch/redis-trib-$VERSION-$RELEASE.amzn2.x86_64.rpm" "centos/7/noarch"
+fi
